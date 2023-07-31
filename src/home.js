5a0574cb197174cad8bf8f300b150da271618f37
Daniel Falcon <Danfall963@Gmail.com>
2023-07-31 04:43:17 -0430

    Routes and Home Banner

--- src/home.js
+++ src/home.js
@@ -10,10 +10,10 @@ const Home = () => {
                 <div className='container'>
                     <div className='detail'>
                         <h2>Los Mejores Productos Al Mejor Precio</h2>
-                        <Link to='/product'>Comprar <BsArrowRight /></Link>
+                        <Link to='/product' className='link'>Comprar <BsArrowRight /></Link>
                     </div>
                     <div className='img_box'>
-                        <img src='./img/SlideImg.avif' alt='SlideImage' />
+                        <img src='./img/SlideImg.png' alt='SlideImage' width="500px" />
                     </div>
                 </div>
             </div>
