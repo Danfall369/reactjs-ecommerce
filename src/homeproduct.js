855986044d42d171f1d048678e401b23056d6ad0
Daniel Falcon <Danfall963@Gmail.com>
2023-07-31 15:03:37 -0430

    Array of the Products

---
+++ src/homeproduct.js
@@ -0,0 +1,430 @@
+const Homeproduct =
+    [
+        {
+            id: 1,
+            title: "Avena Avelina Fortificada",
+            Cat: "Avena",
+            Price: "$54",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/b64b59419959799a6db869732553a98754fbb176-600x600.jpg"
+        },
+        {
+            id: 2,
+            title: "Avena Avelina Hojulas Enteras",
+            Cat: "Avena",
+            Price: "$54",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/a5d2a321778726a3f9ed37ed401bc0bc90ec05f3-600x600.jpg"
+        },
+        {
+            id: 3,
+            title: "Avena Avelina tradicional",
+            Cat: "Avena",
+            Price: "$54",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/31214a8ad5db2afbeb4b9674fc7fca0e3be72570-600x600.jpg"
+        },
+        {
+            id: 4,
+            title: "Avena Avelina Instantanea",
+            Cat: "Avena",
+            Price: "$54",
+            Img: " "
+        },
+        {
+            id: 5,
+            title: "Atun California Dezmenuzado",
+            Cat: "Enlatado",
+            Price: "$45.6",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/74002aaed4039803e36e3dc3550fbf72bc2ab93a-600x600.jpg"
+        },
+        {
+            id: 6,
+            title: "Pasta Capri Pluma",
+            Cat: "Pasta",
+            Price: "$22.92",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/04d8b2f921be4c219533b03947952c279524fa1b-600x600.jpg"
+        },
+        {
+            id: 7,
+            title: "Pasta Capri Rigatoni",
+            Cat: "Pasta",
+            Price: "$22.92",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/8dac3681e6b063236bd7d562b6e8e4c3001f9a71-600x600.jpg"
+        },
+        {
+            id: 8,
+            title: "Pasta Capri Tornillo",
+            Cat: "Pasta",
+            Price: "$22.92",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/d6aa3f81c9e324661149e4d9ead41484e78b9a99-600x600.jpg"
+        },
+        {
+            id: 9,
+            title: "Pasta Capri Vermicelli",
+            Cat: "Pasta",
+            Price: "$22.92",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/36792a6a0b5df625333c8c7689e1db7a7796ca88-600x600.jpg"
+        },
+        {
+            id: 10,
+            title: "Atun Ceiro en Aceite Natural",
+            Cat: "Enlatado",
+            Price: "$30",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/154ccfdfeea9f2e81f2c4bc64168ce9f6f3036f6-600x600.jpg"
+        },
+        {
+            id: 11,
+            title: "Nectar El Tunal de Pera 1L",
+            Cat: "Bebida",
+            Price: "$47",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/ec332ca257ec0752a062b47b6eef8707aff2dd23-510x510.jpg"
+        },
+        {
+            id: 12,
+            title: "Nectar El Tunal de Manzana 1L",
+            Cat: "Bebida",
+            Price: "$47",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/ec332ca257ec0752a062b47b6eef8707aff2dd23-510x510.jpg"
+        },
+        {
+            id: 13,
+            title: "Atun El Valle en Aceite Natural",
+            Cat: "Enlatado",
+            Price: "$47.76",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/cef59df83ea87ab099f2e92c76e306420cf1ee73-600x600.jpg"
+        },
+        {
+            id: 14,
+            title: "Atun Eveba Aleta Amarilla",
+            Cat: "Enlatado",
+            Price: "$69.6",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/b3e5a7e1f5d28ebebdd52edf7e67ce93adef3b8c-600x600.jpg"
+        },
+        {
+            id: 15,
+            title: "Harina Kaly Maiz Blanco",
+            Cat: "Harina",
+            Price: "$19.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/f110422ec2a234580bb6f3210f557fcbc929f391-600x600.jpg"
+        },
+        {
+            id: 16,
+            title: "Azucar Blanca Konfit Refinada",
+            Cat: "Azucar",
+            Price: "$24.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/7ce1a56e976428e6acef5f5b6e7767dc40449d50-600x600.jpg"
+        },
+        {
+            id: 17,
+            title: "Mayonesa Kraft 445Gr",
+            Cat: "Mayonesa",
+            Price: "$43.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/285af3543173d6d707a147e828435b8915e24e3a-600x600.jpg"
+        },
+        {
+            id: 18,
+            title: "Leche La Campesina 800Gr",
+            Cat: "Lacteo",
+            Price: "$108.6",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/890084c827e49c44b759523dc1948922b7c2bc74-400x400.jpg"
+        },
+        {
+            id: 19,
+            title: "Leche La Campiña 900Gr",
+            Cat: "Lacteo",
+            Price: "$115.5",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/6b8f4c0e4f7f0afbd27b2570f26c3eea85903c0e-600x600.jpg"
+        },
+        {
+            id: 20,
+            title: "Pasta La Especial Dedal",
+            Cat: "Pasta",
+            Price: "$19.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/d44ebec66c166a2fc231e4bf5fc48ba3e3d8cc6b-600x600.jpg"
+        },
+        {
+            id: 21,
+            title: "Pasta La Especial Pluma",
+            Cat: "Pasta",
+            Price: "$19.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/8ebc21be16003701bb8dc383b7ac7a3ff80c90d5-600x600.jpg"
+        },
+        {
+            id: 22,
+            title: "Pasta La Especial Tornillo",
+            Cat: "Pasta",
+            Price: "$19.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/8ebc21be16003701bb8dc383b7ac7a3ff80c90d5-600x600.jpg"
+        },
+        {
+            id: 23,
+            title: "Harina Lucharepa Arepas",
+            Cat: "Harina",
+            Price: "$22",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/ba800f0a718c7c80c5c71ab551f186fb631547bc-600x600.jpg"
+        },
+        {
+            id: 24,
+            title: "Atun Margarita en Aceite Vegetal",
+            Cat: "Enlatado",
+            Price: "$66.2",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/c29b403e7a1c21ad4390d30481038bc635632d41-600x600.jpg"
+        },
+        {
+            id: 25,
+            title: "Arroz Mary Dorado",
+            Cat: "Arroz",
+            Price: "$38.4",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/549b41f571094aad18a445c11da5308e245bd9fb-600x600.jpg"
+        },
+        {
+            id: 26,
+            title: "Arroz Mary Esmeralda",
+            Cat: "Arroz",
+            Price: "$42.9",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/a5b6690d4709be29d2becb97a8cc1dda0d5832df-600x600.jpg"
+        },
+        {
+            id: 27,
+            title: "Harina Mary Leudante",
+            Cat: "Harina",
+            Price: "$26",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/30ae9cb2dca62c6e183bac005a6add3b379174e9-600x600.jpg"
+        },
+        {
+            id: 28,
+            title: "Harina Mary Todo Uso",
+            Cat: "Harina",
+            Price: "$23",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/7acb74103866c9f95dbd478db979e6c965cdd1d5-600x600.jpg"
+        },
+        {
+            id: 29,
+            title: "Arroz Mary Integral",
+            Cat: "Arroz",
+            Price: "$37.7",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/ac45ad39dd27f705c940f6ec9f19a7983dcb11e6-600x600.jpg"
+        },
+        {
+            id: 30,
+            title: "Pasta Mary Pluma",
+            Cat: "Pasta",
+            Price: "$16.68",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/d42ef3c0ef8f5fe9a6118f213d4cd84ac69f2c51-600x600.jpg"
+        },
+        {
+            id: 31,
+            title: "Pasta Mary Tornillo",
+            Cat: "Pasta",
+            Price: "$16.68",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/1150c6264d696a866bbfc1b1f1ff5f9d9606c433-600x600.jpg"
+        },
+        {
+            id: 32,
+            title: "Pasta Mary Vermicelli",
+            Cat: "Pasta",
+            Price: "$16.68",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/f877bcd01c357d7de9dfe6a01190bf800ce12709-600x600.jpg"
+        },
+        {
+            id: 33,
+            title: "Arroz Mary Premium",
+            Cat: "Arroz",
+            Price: "$38",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/4b934fede9de762ba3e1192611f95d3a04b53816-600x600.jpg"
+        },
+        {
+            id: 34,
+            title: "Arroz Mary Superior",
+            Cat: "Arroz",
+            Price: "$35.4",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/94ec09de3e1d3ae92d9b714f48d41b26631a22c0-600x600.jpg"
+        },
+        {
+            id: 35,
+            title: "Margarina Mavesa 500gr",
+            Cat: "Margarina",
+            Price: "$30",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/55265e46abd27ef3727c1cba575d725fdff81581-600x600.jpg"
+        },
+        {
+            id: 36,
+            title: "Marganira Mavesa Ligera 500gr",
+            Cat: "Margarina",
+            Price: "$34.2",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/46481a460cf845e81867b34ac0c0e9a21670dfef-600x400.jpg"
+        },
+        {
+            id: 37,
+            title: "Mayonesa Mavesa 910Gr",
+            Cat: "Mayonesa",
+            Price: "$89.76",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/bb40f871671bc7638b96d116ec8c75537042cb30-600x600.jpg"
+        },
+        {
+            id: 38,
+            title: "Aceite Mazeite de Maiz",
+            Cat: "Aceite",
+            Price: "$55.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/e640242f69f5620a4cbc7995db0255cb1bc55f16-600x600.jpg"
+        },
+        {
+            id: 39,
+            title: "Aceite Mediterraneo Intenso",
+            Cat: "Aceite",
+            Price: "$193.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/e32bb453bada03fa2cf6cae150bf35cd9230b067-600x600.jpg"
+        },
+        {
+            id: 40,
+            title: "Aceite Mediterraneo Oliva Extra",
+            Cat: "Aceite",
+            Price: "$196.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/de7e3545c5b2e8bcb960bd71e7f7fbd1ac46abcc-600x600.jpg"
+        },
+        {
+            id: 41,
+            title: "Azucar Montalban Refinada",
+            Cat: "Azucar",
+            Price: "$24.7",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/dae35418caedcab1f4b7d28d031012d0f523edb9-600x600.jpg"
+        },
+        {
+            id: 42,
+            title: "Aceite Naturoil de Soja",
+            Cat: "Aceite",
+            Price: "$27.96",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/eaf2bc3a614d39647dc783fa3038830a684167b8-600x600.jpg"
+        },
+        {
+            id: 43,
+            title: "Marganina Nelly 500Gr",
+            Cat: "Margarina",
+            Price: "$27",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/55461062678a744de0134b7ffd1120d0ecf13107-600x600.jpg"
+        },
+        {
+            id: 44,
+            title: "Atun New Pesca en Aceite Natural",
+            Cat: "Enlatado",
+            Price: "$59.4",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/af2099df832feef5287e27ad8a4a1c9bb2f33713-600x600.jpg"
+        },
+        {
+            id: 45,
+            title: "Atun Nudeli en Aceite Vegetal",
+            Cat: "Enlatado",
+            Price: "$52.8",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/1650dbb6b7b2dd3d6eb4e3edf8f744571e4b0eed-600x600.jpg"
+        },
+        {
+            id: 46,
+            title: "Harina Pan de Maiz Amarillo",
+            Cat: "Harina",
+            Price: "$22",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/ff06d7489f69764c37068cba4d01e7743f386d96-600x600.jpg"
+        },
+        {
+            id: 47,
+            title: "Harina Pan de Maiz Blanco",
+            Cat: "Harina",
+            Price: "$23.2",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/bd269fcf088f4e7d961339d8e900e15f669313e2-600x600.jpg"
+        },
+        {
+            id: 48,
+            title: "Harina Pan de Maiz y Arroz",
+            Cat: "Harina",
+            Price: "$23.4",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/5ea756ebc44c1be023fee3bd0b21c6fa5574712d-600x600.jpg"
+        },
+        {
+            id: 49,
+            title: "Mezcla para Cachapas Pan",
+            Cat: "Harina",
+            Price: "$55.84",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/0e245c543fd3db3dc49cb2d2ebdc70a35e6e6af5-600x600.jpg"
+        },
+        {
+            id: 50,
+            title: "Arroz Primor Clasico",
+            Cat: "Arroz",
+            Price: "$44.5",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/031a35aacab1c564e6996ea6d4c5d08c6770ff82-600x600.jpg"
+        },
+        {
+            id: 51,
+            title: "Pasta Primor Dedales",
+            Cat: "Pasta",
+            Price: "$26.88",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/231587f2ba19ebf4c122626a539c7074a5f9cabf-600x600.jpg"
+        },
+        {
+            id: 52,
+            title: "Pasta Primor Tornillo",
+            Cat: "Pasta",
+            Price: "$25.88",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/0086cabbc72a8c5e0a056da0539f2349917975bb-600x600.jpg"
+        },
+        {
+            id: 53,
+            title: "Pasta Primor Vermicelli",
+            Cat: "Pasta",
+            Price: "$26.88",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/bcb06d10b51e92b92dfe414e56b0cf4f47cc5197-600x600.jpg"
+        },
+        {
+            id: 54,
+            title: "Pasta Ronco Pluma",
+            Cat: "Pasta",
+            Price: "$21.92",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/b1626e83f0e081818302d11ce0dcb443f7890bbc-600x600.jpg"
+        },
+        {
+            id: 55,
+            title: "Pasta Ronco Tornillo",
+            Cat: "Pasta",
+            Price: "$22.92",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/29958c519d1bc1544ac60a531a33f5653a82ef43-600x600.jpg"
+        },
+        {
+            id: 56,
+            title: "Pasta Ronco Vermicelli",
+            Cat: "Pasta",
+            Price: "$21.44",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/fb5f482e11cac348a78376bb41604eba9a1a4cad-600x600.jpg"
+        },
+        {
+            id: 57,
+            title: "Arroz Santoni Excelente",
+            Cat: "Arroz",
+            Price: "$35.1",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/cabb1d73aa3f208c2b872b93013eee9a4efee016-600x600.jpg"
+        },
+        {
+            id: 58,
+            title: "Arroz Santoni Premium",
+            Cat: "Arroz",
+            Price: "$35.1",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/325e093e2b3045036ff5fcdf0c426e013b59e5d5-600x600.jpg"
+        },
+        {
+            id: 59,
+            title: "Arroz Santoni Zafiro",
+            Cat: "Arroz",
+            Price: "$45",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/3dc6eb85ba99197da0bac85b3c911bdcee1b2185-600x600.jpg"
+        },
+        {
+            id: 60,
+            title: "Azucar Sonora Refinada",
+            Cat: "Azucar",
+            Price: "$46.2",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/63e584d1c031b4571529d23ac080b2a41e618647-600x600.jpg"
+        },
+        {
+            id: 61,
+            title: "Aceite Vatel Vegetal",
+            Cat: "Aceite",
+            Price: "$40.2",
+            Img: "https://cdn.sanity.io/images/eml6ymro/production/b8abf5e28874536053ec8b9374ca9450cf195e03-600x600.jpg"
+        }
+    ]
